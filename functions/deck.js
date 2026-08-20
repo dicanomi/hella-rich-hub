7b77d1d87dda8c0d10eab9a82faa8034ab11be8d
dicanomi <jeffrey@trackerandscout.com>
2026-08-20 08:24:51 -0700

    Fix Hella Deck direct route

---
+++ functions/deck.js
@@ -0,0 +1,6 @@
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+  url.pathname = '/index.html';
+
+  return context.env.ASSETS.fetch(new Request(url.toString(), context.request));
+}
